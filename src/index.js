c18857ae6e10964e36fa9c0ca1e18a520ee74f5c
Dmitry <teramisu01@gmail.com>
2019-10-02 01:14:24 +0300

    feat: implement decode

--- src/index.js
+++ src/index.js
@@ -38,7 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    const words = expr.split('**********');
+
+    const decodedWords = words.map(function(word) {
+        const count = word.length / 10;
+
+        const decodedLetters = [];
+
+        for (let index = 0; index < count; index++) {
+            const encodedLetter = word.slice(index * 10 , index * 10 + 10);
+
+            const sounds = [];
+
+            for (let index = 10; index > 0; index = index - 2) {
+                const sound = encodedLetter.slice(index - 2, index);
+
+                if (sound === '10') {
+                    sounds.unshift('.');
+                }
+                if (sound === '11') {
+                    sounds.unshift('-');
+                }
+            }
+
+            const letter = sounds.join('');
+
+            decodedLetters.push(MORSE_TABLE[letter]);
+        }
+
+        return decodedLetters.join('');
+
+    });
+
+    return decodedWords.join(' ');
 }
 
 module.exports = {
